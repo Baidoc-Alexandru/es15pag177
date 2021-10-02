2628fbdc9a60920879c21802ea7b4ebde8033067
Baidoc-Alexandru <alexandru.baidoc@itiszuccante.edu.it>
2021-10-02 21:48:53 +0000

    es15pag177

---
+++ src/es15pag177.java
@@ -0,0 +1,12 @@
+public class es15pag177 {
+    public static void main(String[] args){
+        //inserire valore dell'altezza
+        int altezza;
+        //inserire valore della lunghezza
+        int lunghezza;
+        //inserire valore della profondita
+        int profondita;
+        //calcolare il volume del parallelepipedo
+        System.out.println(altezza*lunghezza*profondita);
+    }
+}
